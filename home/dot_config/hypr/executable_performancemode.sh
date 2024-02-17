a44ebba3fa4e7f2dfcc40c423b847ab08051b192
Nikki Murello <nmurello15@gmail.com>
2024-02-17 12:01:44 -0500

    added discord themes and performance mode script

---
+++ home/dot_config/hypr/executable_performancemode.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+#
+# script grabbed from https://wiki.hyprland.org/Configuring/Uncommon-tips--tricks/
+# 
+
+HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==2{print $2}')
+if [ "$HYPRGAMEMODE" = 1 ] ; then
+    hyprctl --batch "\
+        keyword animations:enabled 0;\
+        keyword decoration:drop_shadow 0;\
+        keyword decoration:blur:enabled 0;\
+        keyword decoration:rounding 0;\
+		keyword general:col.active_border 0xffff0000;\
+		keyword misc:vfr 1"
+    exit
+fi
+hyprctl reload
+
