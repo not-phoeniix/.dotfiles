8f9fad5380fc97d48e541abd7ca0197c0103502f
Nikki Murello <nmurello15@gmail.com>
2024-10-29 22:52:30 -0400

    added weather api code with new weather desktop widget

--- home/dot_config/ags/js/desktop.js
+++ home/dot_config/ags/js/desktop.js
@@ -5,6 +5,8 @@ import { Hour, HourMilit, Minute } from "./variables.js";
 const Name = Variable("[name]");
 const SquareSize = Variable(250);
 const SquareSpacing = Variable(20);
+const WeatherUrlLatitude = Variable(0);
+const WeatherUrlLongitude = Variable(0);
 
 const DigitalClock = Widget.CenterBox({
     className: "panel digital-clock",
@@ -58,10 +60,80 @@ const Greeter = Widget.CenterBox({
     })
 });
 
+const PokemonSearch = Widget.Box({
+    className: "panel",
+    widthRequest: SquareSize.bind(),
+    heightRequest: SquareSize.bind(),
+    vertical: true,
+    setup: self => {
+        const nameLabel = Widget.Label("[pokemon name]");
+        const imageBox = Widget.Box({
+            widthRequest: 200,
+            heightRequest: 200,
+            hexpand: false
+        });
+
+        const searchBox = Widget.Entry({
+            hexpand: true,
+            onAccept: ({ text }) => Utils.fetch(`https://pokeapi.co/api/v2/pokemon/${text}`)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error("Pokemon API response not ok !!!!!");
+                    }
+                    return response.json()
+                }).then(data => {
+                    Utils.fetch(data.forms[0].url).then(response => response.json()).then(form => {
+                        nameLabel.label = `${form.name}: order ${form.order}`;
+                        imageBox.css = `
+                            background-image: url("${form.sprites.front_shiny}");
+                            background-size: contain;
+                        `;
+                    });
+                })
+        });
+
+        self.children = [
+            imageBox,
+            nameLabel,
+            searchBox
+        ];
+    }
+});
+
+const Weather = () => {
+    const tempLabel = Widget.Label("temp: 20°F");
+    const humidLabel = Widget.Label("humid: 0%");
+
+    function refreshWeather() {
+        const url = `https://api.open-meteo.com/v1/forecast?latitude=${WeatherUrlLatitude.value}&longitude=${WeatherUrlLongitude.value}&current=temperature_2m,relative_humidity_2m,apparent_temperature,weather_code,wind_speed_10m&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&forecast_days=3`;
+        Utils.fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                tempLabel.label = `temp: ${data.current.temperature_2m}${data.current_units.temperature_2m}`;
+                humidLabel.label = `humid: ${data.current.relative_humidity_2m}${data.current_units.relative_humidity_2m}`;
+            });
+    }
+
+    return Widget.Box({
+        className: "panel weather",
+        widthRequest: SquareSize.bind(),
+        heightRequest: SquareSize.bind(),
+        vertical: true,
+        children: [
+            tempLabel,
+            humidLabel
+        ]
+    })
+        // weather refresh called every 15 min
+        .poll(60_000 * 15, refreshWeather)
+        .hook(WeatherUrlLongitude, refreshWeather)
+        .hook(WeatherUrlLatitude, refreshWeather);
+}
+
 const WidgetCollection = Widget.Box({
     vpack: "start",
     hpack: "start",
-    css: "margin: 40px;",
+    css: "margin: 30px;",
     spacing: SquareSpacing.bind(),
     vertical: true,
     children: [
@@ -72,6 +144,7 @@ const WidgetCollection = Widget.Box({
                 Greeter
             ]
         }),
+        Weather()
     ]
 });
 
@@ -80,6 +153,7 @@ export const DesktopWidgets = Widget.Window({
     name: `desktop_widgets`,
     anchor: ["top", "bottom", "right", "left"],
     layer: "bottom",
+    keymode: "on-demand",
     child: WidgetCollection,
     setup: self => {
         const Visible = Variable(false);
@@ -118,14 +192,18 @@ export function GetDesktopSettings() {
     return {
         name: Name.value,
         squareSize: SquareSize.value,
-        squareSpacing: SquareSpacing.value
+        squareSpacing: SquareSpacing.value,
+        weatherUrlLatitude: WeatherUrlLatitude.value,
+        weatherUrlLongitude: WeatherUrlLongitude.value
     };
 }
 
 export function LoadDesktopSettings(obj) {
     if (obj) {
-        Name.value = obj.name ?? "[name]";
-        SquareSize.value = obj.squareSize ?? 250;
-        SquareSpacing.value = obj.squareSpacing ?? 20;
+        Name.value = obj.name || "[name]";
+        SquareSize.value = obj.squareSize || 250;
+        SquareSpacing.value = obj.squareSpacing || 20;
+        WeatherUrlLatitude.value = obj.weatherUrlLatitude || 0;
+        WeatherUrlLongitude.value = obj.weatherUrlLongitude || 0;
     }
 }
