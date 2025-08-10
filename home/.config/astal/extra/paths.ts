02f9df9c5227d41eaeb8a9319ab9f330db0023eb
Nikki Murello <nmurello15@gmail.com>
2025-08-10 12:27:30 -0400

    Added absolute pathing for SCSS compilation - Included ~/.cache/wal/ directory in included directories when compiling SASS in the astal config

--- home/.config/astal/extra/paths.ts
+++ home/.config/astal/extra/paths.ts
@@ -8,13 +8,14 @@ const cssCachePath = `${cacheDir}style.css`;
 const configDir = `${GLib.get_user_config_dir()}/desktop/`;
 const settingsFile = `${configDir}config.json`;
 const homeDir = `/home/${GLib.get_user_name()}/`
+const walCacheDir = `${GLib.get_user_cache_dir()}/wal/`;
 
 exec("mkdir -p " + cacheDir);
 
 // set cache path and monitor style file to re-compile css 
 //   every time it changes and hot-reload the program's styling
 function reloadCss() {
-    exec(["sass", "./style.scss", cssCachePath]);
+    exec(["sass", `--load-path=${walCacheDir}`, `./style.scss:${cssCachePath}`]);
     App.reset_css();
     App.apply_css(cssCachePath);
 }
